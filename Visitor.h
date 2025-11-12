86496c23cd16be842c4dd832769b86b432e784b7
nadeenyasser <nadeenyasserelsayed@gmail.com>
2025-11-12 15:21:18 +0200

    Fix Visitor class constructor and member variables

--- Visitor.h
+++ Visitor.h
@@ -5,6 +5,16 @@
 using namespace std;
 
 //define visitor class here
+	private :
+	string visitorname;
+	int ticketsbought;
+	public :
+		Vistor();
+		Vistor(string v, int t);
+		void displayinfo();
+		~Vistor();
+
+};
 
 
 
