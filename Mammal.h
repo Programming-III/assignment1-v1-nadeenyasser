9fb21b5c840c8ff360ab52ad60106fdc83d66802
nadeenyasser <nadeenyasserelsayed@gmail.com>
2025-11-12 15:01:45 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -4,7 +4,17 @@
 #include "Animal.h"
 
 //define mammal class here
-
+class Mammal : public Animal {
+private:
+	string furColor;
+public :
+Mammal();
+Mammal(string f);
+string getfurColor();
+ void setfurColor( string f);
+ ~Mammal();
+	
+};
 
 
 
