bafbd0813a16ab88cc7dc8537bfb909e854c8e94
nadeenyasser <nadeenyasserelsayed@gmail.com>
2025-11-12 15:01:15 +0200

    Add Bird class definition in Bird.h
    Define the Bird class with constructors, getters, and setters.

--- Bird.h
+++ Bird.h
@@ -4,7 +4,16 @@
 #include "Animal.h"
 
 //define class bird here 
-
+class Bird : public Animal {
+	private :
+	float wingSpan;
+	public :
+	Bird();
+	Bird(float w);
+	float getwing();
+	void setwing(float w);
+	~Bird();
+};
 
 
 
